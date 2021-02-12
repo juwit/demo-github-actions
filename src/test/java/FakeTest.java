30a76b06f4bcd336ecd33740a74e897ee49805d9
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-02-12 15:37:53 +0100

    Initial commit

---
+++ src/test/java/FakeTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FakeTest {
+
+    @Test
+    public void testOk(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void testOk2(){
+        assertTrue(true);
+    }
+
+
+}
